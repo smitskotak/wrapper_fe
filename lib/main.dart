a798057ae5d7dc7780d255e93b8781aaae73c76b
Smit Sonani <smit.sonani@kotak.com>
2022-12-26 18:02:10 +0530

    removed path url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 import 'screens/stories_viewer_page.dart';
 
 void main() {
-  setPathUrlStrategy();
   runApp(const MyApp());
 }
 
